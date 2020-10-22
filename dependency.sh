46df6a6518669408e8313a3f8a4a3ac422964895
soumyajeetsahu <68754956+soumyajeetsahu@users.noreply.github.com>
2020-10-22 20:11:01 +0530

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -1,4 +1,6 @@
 apt-get install -y nginx
 pip install gunicorn
 cd /var/www/html
+sudo apt-get remove libapache2-mod-python libapache2-mod-wsgi
+apt-get install libapache2-mod-wsgi-py3
 gunicorn --bind 0.0.0.0:8080 Medi_Co.wsgi:application
